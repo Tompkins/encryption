4054051073fbf3a239ea16a8dd0ecccffdb4a8cb
Tompkins <1011047623@qq.com>
2014-01-06 10:06:25 +0800

    The data file has been changed

--- hashlib_data.py
+++ hashlib_data.py
@@ -1,5 +1,6 @@
 import hashlib
 
-lorem = ''' Lorem ipsum dolor sit amet,
-I can't understand it
-I can'''
+lorem = ''' Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+secd dp eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+enim ad minim
+'''
