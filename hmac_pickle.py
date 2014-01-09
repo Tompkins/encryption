97be3b6947a27cb775a3bce40d296c0664a656f2
Tompkins <1011047623@qq.com>
2014-01-09 20:33:28 +0800

    消息签名的应用，未写完

---
+++ hmac_pickle.py
@@ -0,0 +1,25 @@
+import hashlib
+import hmac
+try:
+    import cPickle as pickle
+except:
+    import pickle
+import pprint
+from StringIO import StringIO
+
+def make_digest(message):
+    '''Return a digest for the message.'''
+    hash = hmac.new('secret-shared-key-goes-here',
+                    message,
+                    hashlib.sha1)
+    return hash.hexdigest()
+
+class SimpleObject(object):
+    '''A very simple class to demonstrate checking digests before
+    unpickling.
+    '''
+    def __init__(self, name):
+        self.name = name
+    def __str__(self):
+        return self.name
+    
