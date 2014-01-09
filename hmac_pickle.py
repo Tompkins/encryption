b20eedbe6d902769c1b45103fdc934a0cf1bdfbc
Tompkins <1011047623@qq.com>
2014-01-09 21:04:26 +0800

    完成hm_pickle.py

--- hmac_pickle.py
+++ hmac_pickle.py
@@ -23,3 +23,49 @@ class SimpleObject(object):
     def __str__(self):
         return self.name
     
+# Simulate a writable socket or pipe with StringIO
+out_s = StringIO()
+
+# Write a valid object to the stream:
+# digest\nlength\npickle
+o = SimpleObject('digest matches')
+pickled_data = pickle.dumps(o)
+digest = make_digest(pickle_data)
+header = '%s %s' % (digest, len(pickled_data))
+print 'WRITING:', header
+out_s.write(header + '\n')
+out_s.write(pickled_data)
+
+# Write an invalid object to the stream
+o = SimpleObject('digest does not match')
+pickled_data = pickle.dumps(o)
+digest = make_digest('not the pickled data at all')
+header = '%s %s' % (digest, len(pickled_data))
+print '\nWRITING:', header
+out_s.write(header + '\n')
+out_s.write(pickled_data)
+
+out_s.flush()
+
+# Simulate a readable socked or pipe with StringIO
+in_s = StringIO(out_s.getvalue())
+
+# Read the data
+while True:
+    first_line = in_s.readline()
+    if not first_line:
+        break
+    incoming_digest, incoming_length = first_line.split(' ')
+    incoming_length = int(incoming_length)
+    print '\nREAD:', incoming_digest, incoming_length
+
+    incoming_pickled_data = in_s.read(incoming_length)
+
+    actual_digest = make_digest(incoming_pickled_data)
+    print 'ACTUAL:', actual_digest
+
+    if incoming_digest != actual_digest:
+        print 'WARNING: Data corruption'
+    else:
+        obj = pickle.loads(incoming_pickled_data)
+        print 'OK:', obj
