21efc147db9d588ed43808210bba00774e737617
Tompkins <1011047623@qq.com>
2014-03-28 21:16:20 +0800

    把reverse_key_value函数放入Morse类中，作为方法使用。

--- morse_code/morse.py
+++ morse_code/morse.py
@@ -1,16 +1,16 @@
+# -*- coding:utf-8 -*-
 from data.morse_dict import sheet
 
-def reverse_key_value(a = {1:'1'}):
-    b = dict()
-    for key in a:
-        b[a[key]] = key
-    return b
-
 class Morse:
-    def __init__(self, sheet = {}):
-        self.sheet = reverse_key_value(sheet)
+    def __init__(self, data = sheet):
+        self.sheet = self.reverse_key_value(data)
     def code(self, data):
         result = ''
         for i in data:
             result += self.sheet[i]
         return result + '00'
+    def reverse_key_value(self, rev_dict = {}):
+        b = dict()
+        for key in rev_dict:
+            b[rev_dict[key]] = key
+        return b
