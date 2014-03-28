85b0af99e4ba5eb29faeb06c54c53f9b6ece1384
Tompkins <1011047623@qq.com>
2014-03-28 20:24:27 +0800

    增加一个更改键-值的函数。

--- morse_code/morse.py
+++ morse_code/morse.py
@@ -1,7 +1,13 @@
 from data.morse_dict import sheet
 
+def reverse_key_value(a = {1:'1'}):
+    b = dict()
+    for key in a:
+        b[a[key]] = key
+    return b
+
 class Morse:
     def __init__(self, sheet = {}):
         self.sheet = sheet
     def code(self, data):
-        
+        pass
