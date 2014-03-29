a64a6ba77ebb3de89179ff8fa59df927f26f4e9c
Tompkins <1011047623@qq.com>
2014-03-29 21:21:21 +0800

    完成decode方法，实现二进制码到字符码的转换。但是转换后没有空格符了。

--- morse_code/morse.py
+++ morse_code/morse.py
@@ -5,9 +5,9 @@ class Morse:
     def __init__(self, data = sheet):
         self.sheet = self.reverse_key_value(data)
         self.sheet_2 = data
-    def code(self, data):
+    def code(self, word):
         result = ''
-        for i in data:
+        for i in word:
             result += self.sheet[i]
         return result + '00'
     def reverse_key_value(self, rev_dict = {}):
@@ -15,3 +15,17 @@ class Morse:
         for key in rev_dict:
             b[rev_dict[key]] = key
         return b
+    def decode(self, data):
+        word_code = []
+        for i in data.split('0000'):
+            if not i == '':
+                word_code.append(i+'00')
+        alpha_code = []
+        for i in word_code:
+            alpha_code.extend(i.split('00'))
+        word = ''
+        for i in alpha_code:
+            if not i == '':
+                word += self.sheet_2[i+'00']
+        return word
+            
